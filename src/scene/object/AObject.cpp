6821ecee786190f2e72a3450b3ddb72b46d220bc
Michael Wydler <michael.wydler@googlemail.com>
2013-05-16 23:05:25 +0200

    Try to fix version compatibility.

--- src/scene/object/AObject.cpp
+++ src/scene/object/AObject.cpp
@@ -87,7 +87,7 @@ void AObject::draw()
 void AObject::drawPostProc()
 {
 	glPushMatrix();
-#if QT_VERSION < 0x05
+#if QT_VERSION < 0x050000
     glMultMatrixd( matrix().constData() );
 #else
     glMultMatrixf( matrix().constData() );
