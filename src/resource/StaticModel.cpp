085a77428782110677082df646ca7baca530f831
Michael Wydler <michael.wydler@googlemail.com>
2013-07-26 18:50:02 +0200

    Create material in StaticModelData to fix memory leak.

--- src/resource/StaticModel.cpp
+++ src/resource/StaticModel.cpp
@@ -10,266 +10,261 @@
 RESOURCE_CACHE(StaticModelData);
 
 
-StaticModelData::StaticModelData( QString name ) :
-    AResourceData( name ),
-    mName( name )
+StaticModelData::StaticModelData( GLWidget * widget, QString name ) :
+	AResourceData( name ),
+	mGLWidget( widget ),
+	mName( name )
 {
 }
 
 
 StaticModelData::~StaticModelData()
 {
-    unload();
+	unload();
 }
 
 
 void StaticModelData::unload()
 {
-    if( !loaded() )
-        return;
-    qDebug() << "-" << this << "StaticModelData" << uid();
+	if( !loaded() )
+		return;
+	qDebug() << "-" << this << "StaticModelData" << uid();
 
-    //TODO: free static data
+	mVertexBuffer.release();
+	mVertexBuffer.destroy();
 
-    StaticModelData::unload();
+	mIndexBuffer.release();
+	mIndexBuffer.destroy();
+
+	StaticModelData::unload();
 }
 
 
 bool StaticModelData::load()
 {
-    unload();
-    qDebug() << "+" << this << "StaticModelData" << uid();
+	unload();
+	qDebug() << "+" << this << "StaticModelData" << uid();
 
-    mParts.clear();
+	parse();
 
-    mVertexBuffer = QGLBuffer( QGLBuffer::VertexBuffer );
-    mVertexBuffer.create();
-
-    mIndexBuffer = QGLBuffer( QGLBuffer::IndexBuffer );
-    mIndexBuffer.create();
-
-    parse();
-
-    return AResourceData::load();
+	return AResourceData::load();
 }
 
 bool StaticModelData::parse()
 {
-    qDebug() << "parse";
+	QFile file( mName );
+	QString line;
+	QString keyword;
+	QStringList fields;
+	QVector<Face> faces;
+	QVector<QVector3D> positions;
+	QVector<QVector2D> texCoords;
+	QVector<QVector3D> normals;
+	QString material;
+	float x, y, z;
+	float u, v;
 
-    QFile file( mName );
-    QString line;
-    QString keyword;
-    QStringList fields;
-    QVector<Face> faces;
-    QVector<QVector3D> positions;
-    QVector<QVector2D> texCoords;
-    QVector<QVector3D> normals;
-    QString material;
-    float x, y, z;
-    float u, v;
+	float minX = 0;
+	float maxX = 0;
+	float minY = 0;
+	float maxY = 0;
 
-    float minX = 0;
-    float maxX = 0;
-    float minY = 0;
-    float maxY = 0;
+	if( !file.open( QIODevice::ReadOnly ) ) {
+		qDebug() << file.errorString();
+		return false;
+	}
 
-    if( !file.open( QIODevice::ReadOnly ) ) {
-        qDebug() << file.errorString();
-        return false;
-    }
+	QTextStream in( &file );
 
-    QTextStream in( &file );
+	while( !in.atEnd() ) {
+		line = in.readLine().trimmed();
 
-    while( !in.atEnd() ) {
-        line = in.readLine().trimmed();
+		while( line.endsWith( "\\" ) )
+		{
+			line.truncate( line.size()-1 );
+			if( in.atEnd() )
+			{
+				break;
+			}
+			line += in.readLine().trimmed();
+		}
 
-        while( line.endsWith( "\\" ) )
-        {
-            line.truncate( line.size()-1 );
-            if( in.atEnd() )
-            {
-                break;
-            }
-            line += in.readLine().trimmed();
-        }
+		if( line.startsWith( "#" ) || line.isEmpty() )
+		{
+			continue;
+		}
 
-        if( line.startsWith( "#" ) || line.isEmpty() )
-        {
-            continue;
-        }
+		fields = line.split( " ", QString::SkipEmptyParts );
+		keyword = fields.takeFirst();
 
-        fields = line.split( " ", QString::SkipEmptyParts );
-        keyword = fields.takeFirst();
+		if( keyword == "v" )
+		{
+			x = fields.takeFirst().toFloat();
+			if( x > maxX )
+				maxX = x;
+			if( x < minX )
+				minX = x;
 
-        if( keyword == "v" )
-        {
-            x = fields.takeFirst().toFloat();
-            if( x > maxX )
-                maxX = x;
-            if( x < minX )
-                minX = x;
+			y = fields.takeFirst().toFloat();
+			if( y > maxY )
+				maxY = y;
+			if( y < minY )
+				minY = y;
 
-            y = fields.takeFirst().toFloat();
-            if( y > maxY )
-                maxY = y;
-            if( y < minY )
-                minY = y;
+			z = fields.takeFirst().toFloat();
+			positions.append( QVector3D( x, y, z ) );
+		}
+		else if( keyword == "vt" )
+		{
+			u = fields.takeFirst().toFloat();
+			v = fields.takeFirst().toFloat();
+			texCoords.append( QVector2D( u, v ) );
+		}
+		else if( keyword == "vn" )
+		{
+			x = fields.takeFirst().toFloat();
+			y = fields.takeFirst().toFloat();
+			z = fields.takeFirst().toFloat();
+			normals.append( QVector3D( x, y, z ) );
+		}
+		else if( keyword == "f" )
+		{
+			Face face;
+			Vertex vertex;
+			QStringList points;
 
-            z = fields.takeFirst().toFloat();
-            positions.append( QVector3D( x, y, z ) );
-        }
-        else if( keyword == "vt" )
-        {
-            u = fields.takeFirst().toFloat();
-            v = fields.takeFirst().toFloat();
-            texCoords.append( QVector2D( u, v ) );
-        }
-        else if( keyword == "vn" )
-        {
-            x = fields.takeFirst().toFloat();
-            y = fields.takeFirst().toFloat();
-            z = fields.takeFirst().toFloat();
-            normals.append( QVector3D( x, y, z ) );
-        }
-        else if( keyword == "f" )
-        {
-            Face face;
-            Vertex vertex;
-            QStringList points;
+			while( !fields.isEmpty() )
+			{
+				points = fields.takeFirst().split( "/" );
+				while( !points.isEmpty() )
+				{
+					vertex.position = positions.at( points.takeFirst().toInt()-1 );
+					vertex.texCoord = texCoords.at( points.takeFirst().toInt()-1 );
+					vertex.normal = normals.at( points.takeFirst().toInt()-1 );
 
-            while( !fields.isEmpty() )
-            {
-                points = fields.takeFirst().split( "/" );
-                while( !points.isEmpty() )
-                {
-                    vertex.position = positions.at( points.takeFirst().toInt()-1 );
-                    vertex.texCoord = texCoords.at( points.takeFirst().toInt()-1 );
-                    vertex.normal = normals.at( points.takeFirst().toInt()-1 );
+					face.points->append( vertex );
+					face.material = material;
+				}
+			}
+			faces.append( face );
+		}
+		else if( keyword == "usemtl" )
+		{
+			QFileInfo fileinfo( file );
+			material = fileinfo.baseName()+"_"+fields.takeFirst();
+		}
+	}
 
-                    face.points->append( vertex );
-                    face.material = material;
-                }
-            }
-            faces.append( face );
-        }
-        else if( keyword == "usemtl" )
-        {
-            QFileInfo fileinfo( file );
-            material = fileinfo.baseName()+"_"+fields.takeFirst();
-        }
-    }
+	file.close();
 
-    file.close();
+	mSize.setWidth(maxX-minX);
+	mSize.setHeight(maxY-minY);
 
-    mSize.setWidth(maxX-minX);
-    mSize.setHeight(maxY-minY);
+	mParts.clear();
+	QString lastMat = faces.first().material;
+	unsigned int current = 0;
+	unsigned int count = 0;
 
-    QString lastMat = faces.first().material;
-    unsigned int current = 0;
-    unsigned int count = 0;
+	foreach( Face face, faces )
+	{
+		if( face.material != lastMat || face == faces.last() )
+		{
+			Part p;
+			p.start = current - count;
+			p.count = count;
+			p.material = new Material( mGLWidget, face.material );
+			mParts.append(p);
 
-    foreach( Face face, faces )
-    {
-        if( face.material != lastMat || face == faces.last() )
-        {
-            Part p;
-            p.start = current - count;
-            p.count = count;
-            p.material = lastMat;
-            mParts.append(p);
+			lastMat = face.material;
+			count = 0;
+		}
 
-            qDebug() << p.start << " + " << p.count << " | " << p.material;
+		foreach( Vertex vertex, *face.points )
+		{
+			if( mVertices.indexOf( vertex ) == -1 )
+			{
+				mVertices.append( vertex );
+			}
+			mIndices.append( mVertices.indexOf( vertex ) );
 
-            lastMat = face.material;
-            count = 0;
-        }
+			current++;
+			count++;
+		}
+	}
 
-        foreach( Vertex vertex, *face.points )
-        {
-            if( mVertices.indexOf( vertex ) == -1 )
-            {
-                mVertices.append( vertex );
-            }
-            mIndices.append( mVertices.indexOf( vertex ) );
+	mVertexBuffer = QGLBuffer( QGLBuffer::VertexBuffer );
+	mVertexBuffer.create();
+	mVertexBuffer.bind();
+	mVertexBuffer.setUsagePattern( QGLBuffer::StaticDraw );
+	mVertexBuffer.allocate( mVertices.constData(), mVertices.size() * sizeof( Vertex ) );
+	mVertexBuffer.release();
 
-            current++;
-            count++;
-        }
-    }
+	mIndexBuffer = QGLBuffer( QGLBuffer::IndexBuffer );
+	mIndexBuffer.create();
+	mIndexBuffer.bind();
+	mIndexBuffer.setUsagePattern( QGLBuffer::StaticDraw );
+	mIndexBuffer.allocate( mIndices.constData(), mIndices.size() * sizeof( unsigned int ) );
+	mIndexBuffer.release();
 
-    qDebug() << mParts.size();
-
-    mVertexBuffer.bind();
-    mVertexBuffer.setUsagePattern( QGLBuffer::StaticDraw );
-    mVertexBuffer.allocate( mVertices.constData(), mVertices.size() * sizeof( Vertex ) );
-    mVertexBuffer.release();
-
-    mIndexBuffer.bind();
-    mIndexBuffer.setUsagePattern( QGLBuffer::StaticDraw );
-    mIndexBuffer.allocate( mIndices.constData(), mIndices.size() * sizeof( unsigned int ) );
-    mIndexBuffer.release();
-
-    return true;
+	return true;
 }
 
 
 StaticModel::StaticModel( GLWidget * widget, QString name ) :
-    AResource(),
-    mGLWidget( widget )
+	AResource(),
+	mGLWidget( widget )
 {
-    QSharedPointer<StaticModelData> n( new StaticModelData( name ) );
-    cache( n );
+	QSharedPointer<StaticModelData> n( new StaticModelData( widget, name ) );
+	cache( n );
 }
 
 
 StaticModel::~StaticModel()
 {
-    //TODO: free data
+	//TODO: free data
 }
 
 void StaticModel::draw()
 {
-    Material * material;
+	data()->vertexBuffer().bind();
+	data()->indexBuffer().bind();
 
-    data()->vertexBuffer().bind();
-    data()->indexBuffer().bind();
+	glEnableClientState( GL_VERTEX_ARRAY );
+	glEnableClientState( GL_TEXTURE_COORD_ARRAY );
+	glEnableClientState( GL_NORMAL_ARRAY );
+	glEnableClientState( GL_INDEX_ARRAY );
 
-    glEnableClientState( GL_VERTEX_ARRAY );
-    glEnableClientState( GL_TEXTURE_COORD_ARRAY );
-    glEnableClientState( GL_NORMAL_ARRAY );
-    glEnableClientState( GL_INDEX_ARRAY );
+	glVertexPointer( 3, GL_FLOAT, sizeof( Vertex ), (void*)offsetof(Vertex,position) );
+	glTexCoordPointer( 2, GL_FLOAT, sizeof( Vertex ), (void*)offsetof(Vertex,texCoord) );
+	glNormalPointer( GL_FLOAT, sizeof( Vertex ), (void*)offsetof(Vertex,normal) );
 
-    glVertexPointer( 3, GL_FLOAT, sizeof( Vertex ), (void*)offsetof(Vertex,position) );
-    glTexCoordPointer( 2, GL_FLOAT, sizeof( Vertex ), (void*)offsetof(Vertex,texCoord) );
-    glNormalPointer( GL_FLOAT, sizeof( Vertex ), (void*)offsetof(Vertex,normal) );
+	foreach( Part part, data()->parts() )
+	{
+		if( part.material )
+		{
+			part.material->bind();
+		}
 
-    foreach( Part part, data()->parts() )
-    {
-        if( part.material != "" )
-        {
-            material = new Material( mGLWidget, part.material );
-            material->bind();
-        }
+		glDrawElements(
+					GL_TRIANGLES,
+					part.count,
+					GL_UNSIGNED_INT,
+					(void*)((size_t)(sizeof(unsigned int)*(	// convert index to pointer
+						part.start		// index to start
+					) ) )
+		);
 
-        glDrawElements(
-                    GL_TRIANGLES,
-                    part.count,
-                    GL_UNSIGNED_INT,
-                    (void*)((size_t)(sizeof(unsigned int)*(	// convert index to pointer
-                        part.start		// index to start
-                    ) ) )
-        );
+		if( part.material )
+		{
+			part.material->release();
+		}
+	}
 
-        if( part.material != "" )
-            material->release();
-    }
+	glDisableClientState( GL_INDEX_ARRAY );
+	glDisableClientState( GL_NORMAL_ARRAY );
+	glDisableClientState( GL_TEXTURE_COORD_ARRAY );
+	glDisableClientState( GL_VERTEX_ARRAY );
 
-    glDisableClientState( GL_INDEX_ARRAY );
-    glDisableClientState( GL_NORMAL_ARRAY );
-    glDisableClientState( GL_TEXTURE_COORD_ARRAY );
-    glDisableClientState( GL_VERTEX_ARRAY );
-
-    data()->vertexBuffer().release();
-    data()->indexBuffer().release();
+	data()->vertexBuffer().release();
+	data()->indexBuffer().release();
 }
--- src/resource/StaticModel.hpp
+++ src/resource/StaticModel.hpp
@@ -13,55 +13,56 @@
 class Face
 {
 public:
-    Face() {
-        points = new QVector<Vertex>();
-    }
+	Face() {
+		points = new QVector<Vertex>();
+	}
 
-    QVector<Vertex> * points;
-    QString material;
+	QVector<Vertex> * points;
+	QString material;
 
-    bool operator==( const Face & other )
-    {
-        return points == other.points
-                && material == other.material;
-    }
+	bool operator==( const Face & other )
+	{
+		return points == other.points
+				&& material == other.material;
+	}
 };
 
 class Part
 {
 public:
-    unsigned int start;
-    unsigned int count;
-    QString material;
+	unsigned int start;
+	unsigned int count;
+	Material * material;
 };
 
 /// The model's data
 class StaticModelData : public AResourceData
 {
 public:
-    StaticModelData( QString name );
-    virtual ~StaticModelData();
+	StaticModelData( GLWidget * widget, QString name );
+	virtual ~StaticModelData();
 
-    const QString & name() const { return mName; }
-    QSizeF size() { return mSize; }
-    QVector<Part> & parts() { return mParts; }
-    QGLBuffer & vertexBuffer() { return mVertexBuffer; }
-    QGLBuffer & indexBuffer() { return mIndexBuffer; }
+	const QString & name() const { return mName; }
+	QSizeF size() { return mSize; }
+	QVector<Part> & parts() { return mParts; }
+	QGLBuffer & vertexBuffer() { return mVertexBuffer; }
+	QGLBuffer & indexBuffer() { return mIndexBuffer; }
 
-    bool parse();
+	bool parse();
 
-    // Overrides:
-    virtual bool load();
-    virtual void unload();
+	// Overrides:
+	virtual bool load();
+	virtual void unload();
 
 protected:
-    QString mName;
-    QSizeF mSize;
-    QVector<Part> mParts;
-    QVector<Vertex> mVertices;
-    QVector<unsigned int> mIndices;
-    QGLBuffer mVertexBuffer;
-    QGLBuffer mIndexBuffer;
+	GLWidget * mGLWidget;
+	QString mName;
+	QSizeF mSize;
+	QVector<Part> mParts;
+	QVector<Vertex> mVertices;
+	QVector<unsigned int> mIndices;
+	QGLBuffer mVertexBuffer;
+	QGLBuffer mIndexBuffer;
 };
 
 
@@ -69,13 +70,13 @@ protected:
 class StaticModel : public AResource<StaticModelData>
 {
 public:
-    StaticModel( GLWidget * widget, QString name );
-    virtual ~StaticModel();
+	StaticModel( GLWidget * widget, QString name );
+	virtual ~StaticModel();
 
-    void draw();
+	void draw();
 
 private:
-    GLWidget * mGLWidget;
+	GLWidget * mGLWidget;
 };
 
 
