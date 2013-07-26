65c46bc5f0b7d1e0456a2a45eb04fc2f70776d2b
zwostein <provisorisch@online.de>
2013-07-26 15:58:54 +0200

    fixed indentation

--- src/resource/AudioSample.cpp
+++ src/resource/AudioSample.cpp
@@ -9,86 +9,88 @@
 #include <float.h>
 
 
-RESOURCE_CACHE(AudioSampleData);
+RESOURCE_CACHE( AudioSampleData );
 
 
 AudioSampleData::AudioSampleData( QString file ) :
-    AResourceData( file ),
-    mFile(file),
-    mBuffer(0)
+	AResourceData( file ),
+	mFile( file ),
+	mBuffer( 0 )
 {
 }
 
 
 AudioSampleData::~AudioSampleData()
 {
-    unload();
+	unload();
 }
 
 
 void AudioSampleData::unload()
 {
-    if( !loaded() )
-        return;
-    qDebug() << "-" << this << "AudioSampleData" << uid();
+	if( !loaded() )
+		return;
 
-    alDeleteBuffers( 1, &mBuffer );
+	qDebug() << "-" << this << "AudioSampleData" << uid();
 
-    AResourceData::unload();
+	alDeleteBuffers( 1, &mBuffer );
+
+	AResourceData::unload();
 }
 
 
 bool AudioSampleData::load()
 {
-    unload();
-    qDebug() << "+" << this << "AudioSampleData" << uid();
+	unload();
+	qDebug() << "+" << this << "AudioSampleData" << uid();
 
-    if( audioLoader( mFile.toLocal8Bit().constData(), &mBuffer, &mFrequency, &mFormat ) != 0 )
-        return false;
+	if( audioLoader( mFile.toLocal8Bit().constData(), &mBuffer, &mFrequency, &mFormat ) != 0 )
+		return false;
 
-    if( mFormat == AL_FORMAT_STEREO8 || mFormat == AL_FORMAT_STEREO16 )
-        qWarning() << "!" << this << "AudioSampleData" << uid() << "is a stereo file - positional audio disabled.";
+	if( mFormat == AL_FORMAT_STEREO8 || mFormat == AL_FORMAT_STEREO16 )
+		qWarning() << "!" << this << "AudioSampleData" << uid() << "is a stereo file - positional audio disabled.";
 
-    return AResourceData::load();
+	return AResourceData::load();
 }
 
 
 AudioSample::AudioSample( QString file ) : AResource()
 {
-    QSharedPointer<AudioSampleData> n( new AudioSampleData( file ) );
-    cache( n );
+	QSharedPointer<AudioSampleData> n( new AudioSampleData( file ) );
+	cache( n );
 
-    mSource = 0;
+	mSource = 0;
 
-    alGenSources( 1, &mSource );
-    if( !mSource )
-    {
-        qFatal("Could not allocate sound source");
-    }
-    alSourcei( mSource, AL_SOURCE_RELATIVE, AL_FALSE );
-    alSourcei( mSource, AL_BUFFER, data()->buffer() );
+	alGenSources( 1, &mSource );
 
-    setLooping( true );
-    setGain( 1.0f );
-    setPitch( 1.0f );
-    setReferenceDistance( 1.0f );
-    setMaxDistance( FLT_MAX );
-    setRolloffFactor( 1.0f );
+	if( !mSource )
+		qFatal( "Could not allocate sound source" );
+
+	alSourcei( mSource, AL_SOURCE_RELATIVE, AL_FALSE );
+	alSourcei( mSource, AL_BUFFER, data()->buffer() );
+
+	setLooping( true );
+	setGain( 1.0f );
+	setPitch( 1.0f );
+	setReferenceDistance( 1.0f );
+	setMaxDistance( FLT_MAX );
+	setRolloffFactor( 1.0f );
 }
 
 
 AudioSample::~AudioSample()
 {
-    if( !mSource )
-        return;
-    alDeleteSources( 1, &mSource );
+	if( !mSource )
+		return;
+
+	alDeleteSources( 1, &mSource );
 }
 
 
 void AudioSample::setPositionAutoVelocity( const QVector3D & position, const double & delta )
 {
-    setPosition( position );
-    QVector3D velocity = (position - mLastPosition) / delta;
-    setVelocity( velocity );
-    mLastPosition = position;
+	setPosition( position );
+	QVector3D velocity = ( position - mLastPosition ) / delta;
+	setVelocity( velocity );
+	mLastPosition = position;
 }
