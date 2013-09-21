7a9a13069b722a67394de66e92b30315c809daf6
Branimir Djordjevic <branimir.djordjevic@gmail.com>
2013-09-21 17:32:31 +0200

    Adding component Splatterbug to project

---
+++ src/scene/object/creature/Splatterbug.hpp
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2013
+ * Branimir Djordjevic <branimir.djordjevic@gmail.com>
+ * Tobias Himmer <provisorisch@online.de>
+ * Michael Wydler <michael.wydler@gmail.com>
+ * Karl-Heinz Zimmermann <karlzimmermann3787@gmail.com>
+ *
+ * This file is part of Splatterlinge.
+ *
+ * Splatterlinge is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * Splatterlinge is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Splatterlinge. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _SPLATTERBUG_HPP_
+#define _SPLATTERBUG_HPP_
+
+class Splatterbug: public ACreature 
+{
+public:
+	;
+
+protected:
+	;
+
+private:
+	;
+
+};
+
+#endif // _SPLATTERBUG_HPP_
+
